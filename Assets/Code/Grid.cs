2d5245252c8ad295714c4d9a5343962b2cb6b5bd
arkenthera <arkengraphics@gmail.com>
2015-06-16 12:23:57 +0300

    Le commit

---
+++ Assets/Code/Grid.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	private int m_iRowCount;
+	private int m_iColumnCount;
+
+	private int m_iGridWidth;
+	private int m_iGridDepth;
+	// Use this for initialization
+	void Start () 
+	{
+		m_iRowCount = 10;
+		m_iColumnCount = 10;
+
+		m_iGridWidth = 10;
+		m_iGridDepth = 20;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		for (int i=0; i < m_iRowCount; i++) {
+			for(int j=0; j < m_iColumnCount; j++)
+			{
+				Vector3 vStart = new Vector3(m_iColumnCount*m_iGridDepth ,0,0 );
+				Vector3 vEnd = new Vector3(m_iColumnCount* m_iGridDepth,10,0);
+
+				Debug.DrawLine(vStart,vEnd);
+			}
+		}
+	}
+}
